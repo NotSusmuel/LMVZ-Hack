1d0e03b2998a9589055e690c8b9553f138bea361
NotSusmuel <166915825+NotSusmuel@users.noreply.github.com>
2024-08-16 09:17:29 +0200

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -89,7 +89,7 @@ function start() {
             var textContent = textElement.textContent;
             var roundMatch = textContent.match(/Du hast Runde \d+ geschafft\./);
             if (roundMatch) {
-                textElement.textContent = `Du hast Runde ${roundCounter} geschafft.(drücke H)`;
+                textElement.textContent = `Du hast Runde ${roundCounter} geschafft. (drücke H)`;
             }
         }
     }
